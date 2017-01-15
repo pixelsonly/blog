53668c21efa4255be81c841d12ac1701c08525b5
Ryan Lindsey <rlindsey@weedmaps.com>
2017-01-15 12:26:57 -0800

    Activate External Pipeline in after_configuration callback

--- environments/production.rb
+++ environments/production.rb
@@ -1,13 +1,17 @@
 configure :production do
   config[:host] = 'https://pixelsonly.com'
 
-  data.articles.article.each do |id, article|
-    proxy "/articles/#{article.slug}/index.html", 'articles/show.html', locals: {article: article }, ignore: true
-  end
+  after_configuration do
+    data.articles.article.each do |id, article|
+      proxy "/articles/#{article.slug}/index.html", 'articles/show.html', locals: {article: article }, ignore: true
+    end
 
-  activate :external_pipeline,
-    name: :webpack,
-    command: './node_modules/webpack/bin/webpack.js --bail -p',
-    source: '.tmp/dist',
-    latency: 0
+    puts "Proxy pages generated successfully!"
+
+    activate :external_pipeline,
+      name: :webpack,
+      command: './node_modules/webpack/bin/webpack.js --bail -p',
+      source: '.tmp/dist',
+      latency: 0
+  end
 end
--- environments/staging.rb
+++ environments/staging.rb
@@ -5,11 +5,13 @@ configure :staging do
     data.articles.article.each do |id, article|
       proxy "/articles/#{article.slug}/index.html", 'articles/show.html', locals: {article: article }, ignore: true
     end
-  end
 
-  activate :external_pipeline,
-    name: :webpack,
-    command: './node_modules/webpack/bin/webpack.js --bail -p',
-    source: '.tmp/dist',
-    latency: 0
+    puts "Proxy pages generated successfully!"
+
+    activate :external_pipeline,
+      name: :webpack,
+      command: './node_modules/webpack/bin/webpack.js --bail -p',
+      source: '.tmp/dist',
+      latency: 0
+  end
 end
