1d72d9f9251d0944b37abc61f116daae8e2e0cee
Ryan Lindsey <rlindsey@weedmaps.com>
2017-08-26 15:49:29 -0700

    basic styles for 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,17 @@
 import React from "react";
+import styled from "styled-components";
+import { Flex, Box } from "grid-styled";
+import { space } from "styled-system";
+import Container from "../components/container";
 
 const NotFoundPage = () =>
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist...SAD!</p>
-  </div>;
+  <Container>
+    <Flex justify="flex-start" wrap>
+      <Box w={[1]} my={[4]}>
+        <h1>404: NOT FOUND</h1>
+        <p>You're looking for a page that doesn&#39;t exist&hellip;SAD!</p>
+      </Box>
+    </Flex>
+  </Container>;
 
 export default NotFoundPage;
